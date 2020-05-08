e85f30ed5879055f46ee7e6943f62a620e8cc827
Jim T <64757897+Gh0st-N1njA@users.noreply.github.com>
2020-05-08 00:04:20 -0500

    Update pow.cpp

--- src/pow.cpp
+++ src/pow.cpp
@@ -135,10 +135,7 @@ bool CheckProofOfWork(uint256 hash, unsigned int nBits)
 
     // Check proof of work matches claimed amount
     if (hash > bnTarget)
-        printf("hash: %s\n", hash.ToString().c_str());
-        printf("bnTarget: %s\n", bnTarget.ToString().c_str());
-        return false;
-       // return error("CheckProofOfWork() : hash doesn't match nBits");
+        return error("CheckProofOfWork() : hash doesn't match nBits");
 
     return true;
 }
