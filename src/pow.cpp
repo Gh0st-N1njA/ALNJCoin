3b9209ecf77d7be2062755dea6313e5aa167055e
Jim T <64757897+Gh0st-N1njA@users.noreply.github.com>
2020-05-08 15:30:38 -0500

    Update pow.cpp

--- src/pow.cpp
+++ src/pow.cpp
@@ -124,7 +124,7 @@ bool CheckProofOfWork(uint256 hash, unsigned int nBits)
     bool fOverflow;
     uint256 bnTarget;
 
-    LogPrintf("%s : nBits: %d \n", __func__, nBits);
+    //LogPrintf("%s : nBits: %d \n", __func__, nBits);
     //return true;
     if (Params().IsRegTestNet()) return true;
 
@@ -136,9 +136,10 @@ bool CheckProofOfWork(uint256 hash, unsigned int nBits)
 
     // Check proof of work matches claimed amount
     if (hash > bnTarget){
-        printf("Hash: %s\n", hash.ToString().c_str());
-        printf("bnTarget: %s\n", bnTarget.ToString().c_str());
-        return error("CheckProofOfWork() : hash doesn't match nBits");
+        //printf("Hash: %s\n", hash.ToString().c_str());
+        //printf("bnTarget: %s\n", bnTarget.ToString().c_str());
+        //return error("CheckProofOfWork() : hash doesn't match nBits");
+        return false;
     }
 
     return true;
