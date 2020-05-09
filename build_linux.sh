7c58c66227ea44c56c5bdbb0b48edba25ff88e59
BuildTools <unconfigured@null.spigotmc.org>
2020-05-09 10:48:05 -0500

    update build and rebuild scripts

--- build_linux.sh
+++ build_linux.sh
@@ -3,9 +3,10 @@ git pull
 NUM_PROCS=`cat /proc/cpuinfo | grep proc | wc -l`
 let "NUM_PROCS--";
 
+make clean;
+./autogen.sh;
+./configure --without-gui;
+
 make -j${NUM_PROCS}
 sudo make install
-alnj-cli stop
-alnj-cli -testnet stop
-rm -rf ~/.alnj
-alnjd -daemon -testnet -debug
+
