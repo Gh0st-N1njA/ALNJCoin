f1d8b26261412e16a2f8bec72c34b179b2ba35ae
Jim T <64757897+Gh0st-N1njA@users.noreply.github.com>
2020-05-09 11:09:12 -0500

    update rebuild script

--- rebuild_linux.sh
+++ rebuild_linux.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+alnj-cli stop
+alnj-cli -testnet stop
 git pull
 NUM_PROCS=`cat /proc/cpuinfo | grep proc | wc -l`
 let "NUM_PROCS--";
 
 make -j${NUM_PROCS}
 sudo make install
-alnj-cli stop
-alnj-cli -testnet stop
+
 rm -rf ~/.alnj
 alnjd -daemon -testnet -debug
