bd7586cf64e54dd42c3354121266b558732a3396
Gh0st-N1njA <64757897+Gh0st-N1njA@users.noreply.github.com>
2020-05-31 00:30:38 -0500

    block adjustments

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -170,18 +170,18 @@ public:
         // height-based activations
         consensus.height_last_PoW = 259200;
         consensus.height_last_ZC_AccumCheckpoint = 1190;
-        consensus.height_last_ZC_WrappedSerials = 1200;
+        consensus.height_last_ZC_WrappedSerials = 1205;
         consensus.height_start_BIP65 = 1;
         consensus.height_start_InvalidUTXOsCheck = 999999999;
         consensus.height_start_MessSignaturesV2 = 1;  // height_start_TimeProtoV2
         consensus.height_start_StakeModifierNewSelection = 1199;
         consensus.height_start_StakeModifierV2 = 1200;
         consensus.height_start_TimeProtoV2 = 1;
-        consensus.height_start_ZC = 1200;
+        consensus.height_start_ZC = 1210;
         consensus.height_start_ZC_InvalidSerials = 999999999;
-        consensus.height_start_ZC_PublicSpends = 1200;
-        consensus.height_start_ZC_SerialRangeCheck = 1200;
-        consensus.height_start_ZC_SerialsV2 = 1200;
+        consensus.height_start_ZC_PublicSpends = 1215;
+        consensus.height_start_ZC_SerialRangeCheck = 1220;
+        consensus.height_start_ZC_SerialsV2 = 1225;
         consensus.height_ZC_RecalcAccumulators = 999999999;
 
         // validation by-pass
